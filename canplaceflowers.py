2e81eac110585d01e4d480d5f90875d2648726d3
safwanislam145 <139922401+safwanislam145@users.noreply.github.com>
2024-07-08 22:04:05 +0000

    first commit

---
+++ canplaceflowers.py
@@ -0,0 +1,39 @@
+"""
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+"""
+
+Class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0:
+                # Check if the current plot is empty 
+                empty_left_plot = (i==0) or (flowerbed[i]-1 = 0)
+                empty_right_plot = (i == len(flowerbed) - 1) or ((flowerbed[i]+1) == 0)
+
+                # If both are empty we can increae count
+                if empty_left_plot and empty_right_plot:
+                    count += 1
+            
+            return count >= n
