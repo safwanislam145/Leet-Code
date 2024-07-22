461033448bc554f04b82b2f6bacd1a2f4d3fabc7
safwanislam145 <139922401+safwanislam145@users.noreply.github.com>
2024-07-22 23:32:54 +0000

    find the difference

---
+++ find_the_difference.py
@@ -0,0 +1,38 @@
+"""
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+Example 2:
+
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+"""
+
+class Solution:
+    def not_in_list1(self, num1, num2):
+        not_in_list = set()
+        exist_in_list2 = set(num2)
+
+        for num in num1:
+            if num not in exist_in_list2:
+                not_in_list.add(num)
+        
+        return list(not_in_list)
+
+    def findDifference(self, nums1, nums2):
+        return (self.not_in_list1(nums1, nums2), self.not_in_list1(nums2, nums1))
