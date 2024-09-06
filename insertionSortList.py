051567f916a8ffa07ed9a63638824d199f2c97c2
safwanislam145 <139922401+safwanislam145@users.noreply.github.com>
2024-09-06 15:13:09 +0000

    insertion sort on linked list

---
+++ insertionSortList.py
@@ -0,0 +1,34 @@
+"""
+Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+
+The steps of the insertion sort algorithm:
+
+Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+It repeats until no input elements remain.
+The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
+"""
+
+#Solution:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = head
+
+        while curr: 
+            prev = dummy 
+            while prev.next and prev.next.val <= curr.val:
+                prev = prev.next
+            
+            next = curr.next
+            curr.next = prev.next
+            prev.next = curr
+            curr = next
+        
+        return dummy.next
